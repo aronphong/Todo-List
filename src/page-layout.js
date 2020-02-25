f4206ef5073e816b1a2cc54fd3aa9baf70128307
aron <aronphong@gmail.com>
2020-02-25 06:07:58 -0800

    Create function to render titles to DOM

--- src/page-layout.js
+++ src/page-layout.js
@@ -1,12 +1,37 @@
 const renderPageContent = () => {
 
     const container = document.querySelector("#container");
+
     console.log(container);
 
-    // const navBar = document.createElement("nav");
+
+
 
 
     // container.appendChild(navBar);
 }
 
-export { renderPageContent };
+const renderProjectTitles = (arr) => {
+
+    const sideBar = document.querySelector("#side-bar");
+
+    for (let i = 0; i < arr.length ; i++) {
+        let newItem = document.createElement("div");
+        newItem.className = "project-item";
+
+        let itemTitle = document.createElement("h1");
+        itemTitle.className = "project-title"
+        itemTitle.textContent = arr[i].title;
+
+        let itemLogo = document.createElement("div");
+        itemLogo.className = "project-logo";
+
+        newItem.appendChild(itemLogo);
+        newItem.appendChild(itemTitle);
+        sideBar.appendChild(newItem);
+    }
+
+}
+
+
+export { renderPageContent, renderProjectTitles };
